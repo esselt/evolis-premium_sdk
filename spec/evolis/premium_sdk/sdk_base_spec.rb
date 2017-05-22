2bda2170e8c5a77ef5e8bddf7dc1a1e12d5ca01f
Boye Holden <boye.holden@ntnu.no>
2017-05-22 14:54:13 +0200

    Started the supervision testing

--- spec/evolis/premium_sdk/sdk_base_spec.rb
+++ spec/evolis/premium_sdk/sdk_base_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 module Evolis::PremiumSdk
-  RSpec.describe Print do
+  RSpec.describe SdkBase do
     # Unsure of how to properly test this class
   end
 end
--- spec/evolis/premium_sdk/supervision_spec.rb
+++ spec/evolis/premium_sdk/supervision_spec.rb
@@ -8,6 +8,18 @@ module Evolis::PremiumSdk
       expect(resource).to be_kind_of(Supervision)
     end
 
-    # Don't know how to test with persistent sessions, will come back later
+    describe '#list' do
+      it 'return array of printers' do
+        expect(resource.list 'Evolis Primacy').to be_kind_of(Array)
+      end
+
+      it 'unknown model returns empty list' do
+        expect(resource.list '__ERROR__').to be_empty
+      end
+
+      it 'level over 2 raise InvalidStateLevelError' do
+        expect{resource.list 'Evolis Primacy', 3}.to raise_error(Error::InvalidStateLevelError)
+      end
+    end
   end
 end
